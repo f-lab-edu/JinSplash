4fed5711d5758b6b1880f329ef50b9f8b5fa4c33
nara04040 <nara04040@naver.com>
2023-12-07 00:50:41 +0900

    feat : 무한 스크롤 이벤트 구현

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,36 +1,89 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { styled } from 'styled-components'
 import GridImgComponent from './GridImgComponent'
 
+interface UrlsType {
+	raw: string
+	full: string
+	regular: string
+	small: string
+	thumb: string
+}
+
+interface ApiResponse {
+	alt_description: string
+	blur_hash: string
+	color: string
+	created_at: string
+	height: number
+	id: string
+	promoted_at: string
+	slug: string
+	urls: UrlsType
+	width: number
+}
+
 export default function MainComponent() {
-	const [responseData, setResponseData] = useState<null | []>(null)
+	const [responseData, setResponseData] = useState<null | [ApiResponse]>(null)
+	const [measuredImgHeight, setMeasuredHeight] = useState<number[]>([0, 0, 0])
+
 	const fetchRequest = async () => {
+		console.log('data를 fetch합니다.')
 		const response = await fetch(
 			`https://api.unsplash.com/photos/?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`,
 		)
 		const responseJson = await response.json()
-		setResponseData(responseJson)
+		setResponseData((prevData) =>
+			prevData ? [...prevData, ...responseJson] : responseJson,
+		)
 	}
+
+	const calculateHeight = () => {
+		let imgHeightData = []
+		let spliceData = []
+		if (responseData) {
+			for (let i = 0; i < responseData.length; i++) {
+				imgHeightData.push(responseData[i].height)
+			}
+		}
+		for (let j = 0; j <= imgHeightData.length; j++) {
+			spliceData.push(imgHeightData.splice(0, 3))
+		}
+
+		// console.log(spliceData)
+	}
+	calculateHeight()
+
+	// 첫 이미지 렌더링
 	useEffect(() => {
-		fetchRequest()
+		const initFetch = async () => {
+			try {
+				await fetchRequest()
+			} catch (error) {
+				console.error('데이터 로드 중 에러 발생 : ', error)
+			}
+		}
+		initFetch()
 	}, [])
-	// console.log(responseData)
+
+	// 스크롤 이벤트 리스너
+	useEffect(() => {
+		const onScroll = () => {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+				fetchRequest()
+			}
+		}
+
+		window.addEventListener('scroll', onScroll)
+
+		return () => window.removeEventListener('scroll', onScroll)
+	}, [responseData])
 
 	return (
 		<JinspalshImgContainer>
 			<JinsplashImgWrapper>
 				<JinspalshImgBox>
 					<GridBox>
-						<GridItemWrapper>
-							{responseData?.map((data, key) => (
-								<GridImgComponent data={data} key={key} />
-							))}
-						</GridItemWrapper>
-						<GridItemWrapper>
-							{responseData?.map((data, key) => (
-								<GridImgComponent data={data} key={key} />
-							))}
-						</GridItemWrapper>
 						<GridItemWrapper>
 							{responseData?.map((data, key) => (
 								<GridImgComponent data={data} key={key} />
